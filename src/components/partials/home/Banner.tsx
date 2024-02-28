b159375dc732aae0cec1a09cf9a5543dc52fd11a
chirag <chirag.gohil@02h.com>
2024-02-28 15:24:06 +0530

    fix: banner slider issue

--- src/components/partials/home/Banner.tsx
+++ src/components/partials/home/Banner.tsx
@@ -4,11 +4,12 @@ import { Swiper, SwiperSlide } from "swiper/react"
 import { Navigation, Autoplay, Pagination, A11y } from 'swiper/modules'
 
 // Utils
-const SwiperNavigation = lazy(() => import('../../common/Utils').then((module) => ({ default: module.SwiperNavigation })))
+// const SwiperNavigation = lazy(() => import('../../common/Utils').then((module) => ({ default: module.SwiperNavigation })))
 import useApiRequest from "@/hooks/useAPIRequest"
 import { Url } from "url"
 import { ENDPOINTS } from "@/utils/constants"
 import { GatsbyImage, StaticImage } from "gatsby-plugin-image"
+import { SwiperNavigation } from "@/components/common/Utils"
 
 interface IbannerData {
   id: number,
@@ -26,7 +27,7 @@ function Banner() {
   const isLargeScreen = useMediaQuery((theme: Theme) => theme.breakpoints.up('lg'));
   const isMobile = useMediaQuery((theme: any) => theme.breakpoints.down('sm'))
   const [tempImgHide, setTempImgHide] = useState(true)
-  const [config, setConfig] = useState({
+  const config = {
     slidesPerView: 1,
     spaceBetween: 30,
     navigation: {
@@ -44,14 +45,13 @@ function Banner() {
       draggable: true
     },
     grabCursor: true,
-    // autoplay: {
-    //   delay: 4000,
-    // },
-  })
+    autoplay: {
+      delay: 8000,
+    },
+  }
   useEffect(() => {
     const x = setTimeout(() => {
       setTempImgHide(false)
-      setConfig((prev) => ({ ...prev, autoplay: { delay: 3000 } }))
     }, 2000);
     return () => {
       clearTimeout(x)
@@ -62,8 +62,8 @@ function Banner() {
     <Box id="Banner" component="section" key={'banner'}>
       <Typography variant="h2" className="BannerTitle">Top articles</Typography>
       <Box className="SwiperContainer">
-        <Swiper {...config} >
-          {data?.data?.length > 0 ?
+        {data?.data?.length > 0 ?
+          <Swiper {...config} >
             <>
               {
                 data?.data?.map((item: IbannerData, index: number) => {
@@ -71,7 +71,7 @@ function Banner() {
                     <SwiperSlide key={`BannerSlider-${index}`}>
                       <Box className="Wrapper" sx={{ position: 'relative', width: '100%', height: '100%' }}>
                         {<>
-                        {/* <StaticImage
+                          {/* <StaticImage
                           rel="prefetch"
                           loading="eager"
                           src={'../../../assets/images/loading.gif'}
@@ -79,12 +79,12 @@ function Banner() {
                           alt="background"
                           style={{visibility: tempImgHide ?'visible' :'hidden', position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'fill' }}
                         />   */}
-                        <img
+                          <img
                             rel="prefetch"
                             loading="eager"
                             src={isLargeScreen ? item.cdnUrlLarge : item.cdnUrlSmall}
                             alt="background"
-                            style={{ visibility: !tempImgHide ?'visible' :'hidden',position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'fill' }}
+                            style={{ visibility: !tempImgHide ? 'visible' : 'hidden', position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'fill' }}
                           /></>}
                       </Box>
                     </SwiperSlide>
@@ -92,13 +92,13 @@ function Banner() {
                 })
               }
             </>
+            </Swiper>
             :
             <>
               {!isMobile ? <Skeleton animation="wave" height="75vh" width="100vw" style={{ transform: "none", margin: "auto", borderRadius: "0px" }} /> : <Skeleton animation="wave" height="300px" width="100vw" style={{ transform: "none", margin: "auto", borderRadius: "0px" }} />}
             </>
           }
-        </Swiper>
-        {data?.data?.length > 0 ? <Suspense fallback={<></>}><SwiperNavigation /></Suspense> : null}
+        {<SwiperNavigation />}
       </Box>
     </Box >
   )
