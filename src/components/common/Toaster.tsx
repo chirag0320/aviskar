c755c60db1eebda5556b1f3b512096468c9c6997
Hardik Makwana <hardik.makwana@o2h.com>
2024-03-21 12:38:08 +0530

    feat: add toaster

---
+++ src/components/common/Toaster.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react"
+import Button from "@mui/material/Button"
+import Snackbar from "@mui/material/Snackbar"
+import Alert from "@mui/material/Alert"
+
+interface Toaster {
+  children: any
+}
+
+function Toaster(props: Toaster) {
+  const { children } = props
+  const [open, setOpen] = useState<boolean>(false)
+
+  const handleClick = () => {
+    setOpen(true)
+  }
+
+  const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === "clickaway") {
+      return
+    }
+
+    setOpen(false)
+  }
+
+  return (
+    <>
+      {/* <Button onClick={handleClick}>Open Snackbar</Button> */}
+      <Snackbar
+        open={open}
+        onClose={handleClose}
+        autoHideDuration={6000}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      >
+        <Alert
+          onClose={handleClose}
+          severity="info"
+          variant="filled"
+        >
+          {children}
+        </Alert>
+      </Snackbar>
+    </>
+  )
+}
+
+export default Toaster
--- src/pages/product-details/[product-friendlyName].tsx
+++ src/pages/product-details/[product-friendlyName].tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react"
-import { Container } from "@mui/material"
+import { Button, Container } from "@mui/material"
 
 // Components
 import Layout from "@/components/common/Layout"
@@ -12,6 +12,7 @@ import { getProductDetailsData } from "@/redux/reducers/categoryReducer"
 import { ENDPOINTS } from "@/utils/constants"
 import { useAppDispatch, useAppSelector } from "@/hooks"
 import { setRecentlyViewedProduct } from "@/redux/reducers/homepageReducer"
+import Toaster from "@/components/common/Toaster"
 
 function ProductDetail({ params }: any) {
   const { productDetailsData } = useAppSelector((state) => state.category)
@@ -21,7 +22,7 @@ function ProductDetail({ params }: any) {
     )
   })
   useEffect(() => {
-    if(productDetailsData?.productId){
+    if (productDetailsData?.productId) {
       dispatch(setRecentlyViewedProduct(productDetailsData?.productId))
     }
   }, [productDetailsData?.productId])
@@ -35,6 +36,9 @@ function ProductDetail({ params }: any) {
       />
       <Breadcrumb page1={"Shop"} page2={"Products"} page3={"2024 1oz Lunar Series III Year of the Dragon Silver Coin"} />
       <Container id="PageProductDetail">
+        <Toaster>
+          The product has been added to your <Button href="#">product comparison</Button>
+        </Toaster>
         {productDetailsData?.productId && <AboutProduct productId={productDetailsData?.productId} />}
         {productDetailsData?.relatedProducts?.length > 0 && <RelatedProduct relatedProductsList={structuredClone(productDetailsData?.relatedProducts)} />}
       </Container>
