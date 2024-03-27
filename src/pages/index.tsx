450b7b2b996d40ebfb96ee4844cbab216ccbe3d4
vipul chaudhary <vipschaudhary044@gmail.com>
2024-03-27 18:13:05 +0530

    fix: success message for copied to clipboard a url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,6 +19,7 @@ import Toaster from "@/components/common/Toaster";
 function IndexPage() {
   const dispatch = useAppDispatch()
   const { configDetails: configDetailsState, openToaster,scrollPosition } = useAppSelector((state) => state.homePage)
+  console.log("🚀 ~ IndexPage ~ scrollPosition:", scrollPosition)
   const isMobile = useMediaQuery((theme: any) => theme.breakpoints.down('md'))
 
   const [body] = useState({
