61374a6add28bbc831025667d1d646cb8d338088
Hardik Makwana <hardik.makwana@o2h.com>
2024-03-28 16:30:25 +0530

    feat: add fontawesome cdn

--- src/components/common/Seo.tsx
+++ src/components/common/Seo.tsx
@@ -70,7 +70,9 @@ function Seo({ description, lang, meta, keywords, title }: any) {
         .concat(filteredMeta)}
       title={title}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
-    />
+    >
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+    </Helmet>
   )
 }
 
