c82cdb553214b8f73c8098f03a06e2165d6897b6
vipul chaudhary <vipschaudhary044@gmail.com>
2024-03-30 21:13:50 +0530

    fix: shopping cart unneccessary re-render stopped

--- src/components/partials/shopping-cart/ShoppingCartComponent.tsx
+++ src/components/partials/shopping-cart/ShoppingCartComponent.tsx
@@ -2,17 +2,25 @@ import React, { useState } from 'react'
 import CartDetails, { CartItemsWithLivePriceDetails } from './CartDetails'
 import CartOrderSummary from './CartOrderSummary'
 import { Box } from '@mui/material'
+import { useAppSelector } from '@/hooks'
+import Toaster from '@/components/common/Toaster'
+
 
 const ShoppingCartComponent = () => {
+    const openToaster = useAppSelector(state => state.homePage.openToaster)
+
     const [cartItemsWithLivePrice, setCartItemsWithLivePrice] = useState<CartItemsWithLivePriceDetails[]>([]);
     const [quantities, setQuantities] = useState<{ [key: number]: number }>({})
 
     return (
-        <Box className="ShoppingCartContent">
-            <CartDetails cartItemsWithLivePrice={cartItemsWithLivePrice} setCartItemsWithLivePrice={setCartItemsWithLivePrice} quantities={quantities} setQuantities={setQuantities} />
-            <CartOrderSummary cartItemsWithLivePrice={cartItemsWithLivePrice} quantities={quantities} />
-        </Box>
+        <>
+            {openToaster && <Toaster />}
+            <Box className="ShoppingCartContent">
+                <CartDetails cartItemsWithLivePrice={cartItemsWithLivePrice} setCartItemsWithLivePrice={setCartItemsWithLivePrice} quantities={quantities} setQuantities={setQuantities} />
+                <CartOrderSummary cartItemsWithLivePrice={cartItemsWithLivePrice} quantities={quantities} />
+            </Box>
+        </>
     )
 }
 
-export default ShoppingCartComponent
+export default React.memo(ShoppingCartComponent);
