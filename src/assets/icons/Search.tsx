36f1e37f1cc3ca6d5c0416a5a1fdf4bbd95058fb
Hardik Makwana <hardik.makwana@o2h.com>
2024-04-01 12:17:44 +0530

    feat: header - update searchField ui with search button

--- src/assets/icons/Search.tsx
+++ src/assets/icons/Search.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { SvgIcon, type SvgIconProps } from "@mui/material"
 
-function Search() {
+function Search(props: SvgIconProps) {
   return (
-    <svg width="21" height="22" viewBox="0 0 21 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="9.88431" cy="10.3775" r="8.98856" stroke="#1D2129" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-      <path d="M16.136 17.0959L19.66 20.6108" stroke="#1D2129" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-    </svg>
+    <SvgIcon width="21" height="22" viewBox="0 0 21 22" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <circle cx="9.88431" cy="10.3775" r="8.98856" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M16.136 17.0959L19.66 20.6108" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+    </SvgIcon>
   )
 }
 
