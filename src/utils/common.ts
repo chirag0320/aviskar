4628d103230090fa9fcb024c929e9c92d3ad4981
dhruvil raval <dhruvilkumar.raval@o2h.com>
2024-04-04 16:51:51 +0530

    Bug-Fix: PaymentMethodType Credit Card and Bank Transfer

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -12,6 +12,7 @@ export const stockStatus: any = {
   profit: 'profit',
   loss: 'loss',
 }
+
 export function formatDate(dateString: any) {
   const options: any = { year: 'numeric', month: 'long', day: 'numeric' };
   const date = new Date(dateString);
@@ -25,16 +26,19 @@ export function progressBarLogic({ currentprice, min, max }: any) {
 export function valueChangeForPrice({ currentprice, yesterdayprice }: { currentprice: number, yesterdayprice: number }) {
   return (((currentprice - yesterdayprice) / yesterdayprice) * 100).toFixed(2)
 }
+
 export function roundOfThePrice(price: number) {
   // return Math.round((price + Number.EPSILON) * 100) / 100
   // return (price).toFixed(2);
   return (Math.round((price + Number.EPSILON) * 100) / 100).toFixed(2);
 }
+
 export const shipmentTypeToEnum: any = {
   'LocalShipping': 3,
   'SecureShipping': 2,
   'VaultStorage': 1
 }
+
 export const isBrowser = typeof window !== "undefined"
 export function localStorageGetItem(key: any) {
   return isBrowser && key ? (localStorage?.getItem(key) === 'undefined' ? undefined : localStorage?.getItem(key)) : undefined
@@ -43,12 +47,13 @@ export function localStorageGetItem(key: any) {
 export function localStorageSetItem(key: any, value: any) {
   isBrowser && localStorage?.setItem(key, typeof value !== 'string' ? JSON.stringify(value) : value)
 }
+
 export function hasFulfilled(dataType: string): boolean {
   return dataType.includes('/fulfilled');
 }
 
 // Function to store the last page in session storage
-export const storeLastPage = (pageUrl:string) => {
+export const storeLastPage = (pageUrl: string) => {
   sessionStorage.setItem('lastPage', pageUrl);
 };
 
@@ -56,11 +61,22 @@ export const storeLastPage = (pageUrl:string) => {
 export const getLastPage = () => {
   return sessionStorage.getItem('lastPage');
 };
-export const bodyForGetShoppingCartData={
+export const bodyForGetShoppingCartData = {
   "search": "",
   "pageNo": 0,
   "pageSize": -1,
   "sortBy": "",
   "sortOrder": "",
   "filters": {}
+}
+
+export function getDefaultOption(enabledOptions: any[], defaultOption: string | number) {
+  const enabledValues = enabledOptions.filter(option => option.enabled).map(option => option.value);
+  return enabledValues.length > 0 ? enabledValues[0] : defaultOption;
+}
+
+export const paymentMethodType:any = {
+  "BankTransfer": 'Bank Transfer',
+  "Cash": 'Cash',
+  "CreditCard": 'Credit Card'
 }
