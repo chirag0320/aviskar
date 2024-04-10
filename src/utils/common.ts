0250fae50e6571a56f475fd19f42459510434da9
chirag <chirag.gohil@02h.com>
2024-04-10 15:44:53 +0530

    fix: conflicts not properly taken so that issue is solved

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1337,11 +1337,7 @@ export const PhoneNumberCountryCode = [
     "code": "ZW"
   }
 ]
-<<<<<<< HEAD
 export const checkThePopUpDetails = async (paramsObj: IPopUpDetails, openPopup: any, dispatch: any, service: any) => {
-=======
-export const checkThePopUpDetails = async (paramsObj: IPopUpDetails, openPopup: any, dispatch:any, service:any) => {
->>>>>>> Staging
   // const res = await ConfigServices.getPopUpDetails(paramsObj)
   const res = await dispatch(service(paramsObj))
   console.log("🚀 ~ checkThePopUpDetails ~ res:", res)
