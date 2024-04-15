b37ef2b6c7e0735ad5a8217d10b3c21d398db471
chirag <chirag.gohil@02h.com>
2024-04-15 11:04:14 +0530

    error due to conflicts are solved

--- src/redux/reducers/myVaultReducer.ts
+++ src/redux/reducers/myVaultReducer.ts
@@ -508,6 +508,7 @@ export const myVaultSlice = createSlice({
             });
 
             state.privateHoldingFormDropdowns = privateHoldingFormDropdowns;
+        })
         // send for equiry
         builder.addCase(sendForEnquiry.pending, state => {
             state.loading = true;
